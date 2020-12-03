66a50db49879cbc5bed4584eaf29dfc3d0584c85
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-12-02 20:29:53 -0600

    basic set up with express in sever file

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const db = require("./database")
+
+const server = express()
+
+server.use(express.json)
+
+
+
+server.listen(8080, () => {
+	console.log("server started")
+})
