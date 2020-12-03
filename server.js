fc3185eeb9195a16643395ad9f126318969f7132
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-12-02 21:30:02 -0600

    completed post endpoint

--- server.js
+++ server.js
@@ -31,15 +31,35 @@ server.get("/users/:id", (req, res) => {
         res.json(user)
     } else {
         res.status(500).json({
-            errorMessage: "The user information could not be retrieved."
+            errorMessage: "The user information could not be retrieved.",
         })
     }
 
     if(!id) {
         return res.status(404).json({
-            message: "The user with the specified ID does not exist."
+            message: "The user with the specified ID does not exist.",
         })
     }
+})
+
+server.post("/users", (req, res) => {
+    const newUser = db.createUser({
+        name: req.body.name,
+        bio: req.body.bio,
+    })
+	if (!req.body.name || !req.body.bio) {
+		return res.status(400).json({
+			errorMessage: "Please provide name and bio for the user.",
+		})
+	} if (newUser) {
+        res.status(201).json(newUser)
+    } else {
+        res.status(500).json({
+            errorMessage: "There was an error while saving the user to the database",
+        })
+    }
+
+	res.json(newUser)
 })
 
 server.listen(8080, () => {
